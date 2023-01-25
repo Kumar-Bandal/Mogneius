ee82c447be7d1f6431f8e7e95413e891be2c1701
Kumar-Bandal <123502941+Kumar-Bandal@users.noreply.github.com>
2023-01-25 14:28:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,4 +6,4 @@ app = Flask(__name__)
 def hello_world():
    return 'LazyDeveloper'
 
-os.system("git clone https://LazyDeveloperr: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@github.com/LazyDeveloperr/Lazyv2testbot okk && cd okk && pip3 install -U -r requirements.txt && nohup python3 bot.py &")
+os.system("git clone https:///Kumar-Bandal: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@github.com/Kumar-Bandal/Bot okk && cd okk && pip3 install -U -r requirements.txt && nohup python3 bot.py &")
